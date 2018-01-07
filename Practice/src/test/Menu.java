96b89a8466f64c584f31903c9ba25fdd51023565
heedeandean <7942ahj@hanmail.net>
2018-01-07 23:05:15 +0900

    2018-01-07

---
+++ Practice/src/test/Menu.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Menu {
+	public static final int SAME = 4000;
+	public static final int SLATTE = 4500;
+	
+	public static final int BAME = 4000;
+	public static final int BLATTE = 4500;
+	
+}

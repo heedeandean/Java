6595bc9e7237b714b6d556bfd919f87423aaf12d
heedeandean <7942ahj@hanmail.net>
2018-11-03 23:43:00 +0900

    continue문 예제

--- Practice/src/study/HelloJava.java
+++ Practice/src/study/HelloJava.java
@@ -4,6 +4,15 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		
-
+		int sum = 0;
+		int i = 1;
+		
+		for (; i<=100; i++) {
+			if (i % 2 == 0) 
+				continue;
+			
+			sum += i;
+		}
+		System.out.println(sum);
 	}
 }

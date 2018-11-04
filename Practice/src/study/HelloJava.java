2c91a278d015a71c2e4b0ccefc41168f2dc391f0
heedeandean <7942ahj@hanmail.net>
2018-11-05 00:14:16 +0900

    break문

--- Practice/src/study/HelloJava.java
+++ Practice/src/study/HelloJava.java
@@ -3,7 +3,16 @@ package study;
 public class HelloJava {
 
 	public static void main(String[] args) {
+		int sum = 0;
+		int i;
 		
-	
+		for(i=0; ; i++) {
+			sum += i;
+			
+			if(sum >= 100)
+				break;
+		}
+		System.out.println(i);
+		System.out.println(sum);
 	}
 }

923e75016044f4be3c86534f5fe973860d6cc12c
heedeandean <7942ahj@hanmail.net>
2018-11-07 00:28:04 +0900

    break문

--- Practice/src/study/BreakTest.java
+++ Practice/src/study/BreakTest.java
@@ -1,17 +1,21 @@
 package study;
 
-public class HelloJava {
+public class BreakTest {
 
 	public static void main(String[] args) {
-		int sum = 0;
-		int i;
 		
-		for(i=0; ; i++) {
+		// 1부터 더했을 때 그 합이 500이 넘는 자연수는?
+		int i;
+		int sum = 0;
+		
+		for(i=1; ; i++) {
+			
 			sum += i;
 			
-			if(sum >= 100)
+			if(sum > 500)
 				break;
 		}
+		
 		System.out.println(i);
 		System.out.println(sum);
 	}

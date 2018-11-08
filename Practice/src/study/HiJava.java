68f9e6dff5ca221f114fee110e835e275855ca33
heedeandean <7942ahj@hanmail.net>
2018-11-09 00:47:49 +0900

    if-else if문

--- Practice/src/study/HiJava.java
+++ Practice/src/study/HiJava.java
@@ -3,8 +3,26 @@ package study;
 public class HiJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num1 = 10;
+		int num2 = 2;
+		
+		char operator = '*';
+		
+		if(operator == '+') {
+			System.out.println(num1 + num2);
+		}
+		else if(operator == '-') {
+			System.out.println(num1 - num2);
+		}
+		else if(operator == '*') {
+			System.out.println(num1 * num2);
+		}
+		else if(operator == '/') {
+			System.out.println(num1 / num2);
+		}
+		else {
+			System.out.println("존재하지 않는 사칙연산");
+		}
 
 	}
-
 }

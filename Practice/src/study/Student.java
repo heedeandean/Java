66e664ea0cc34386effab54fcedb460411fcd31e
heedeandean <7942ahj@hanmail.net>
2018-11-14 03:08:10 +0900

    메서드

---
+++ Practice/src/study/Student.java
@@ -0,0 +1,17 @@
+package study;
+
+public class Student {
+	int studentID;
+	String studentName = "희딘딘";
+	int grade;
+	String address = "캐나다 벤쿠버";
+	
+	
+	// 학생 이름과 주소 출력하는 메서드.
+	public void stuInfo() {
+		System.out.println(studentName + " : " + "address");
+	}
+	
+	
+	
+}

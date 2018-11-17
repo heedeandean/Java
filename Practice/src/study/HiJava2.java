51d908c294db6f23955a6f092df21f3f339b534f
heedeandean <7942ahj@hanmail.net>
2018-11-17 20:33:53 +0900

    인스터스 여러개 생성하기

---
+++ Practice/src/study/HiJava2.java
@@ -0,0 +1,16 @@
+package study;
+
+public class HiJava2 {
+
+	public static void main(String[] args) {
+		HiJava s1 = new HiJava();
+		s1.studentName = "»ÒµÚµÚ";
+		System.out.println(s1.getStudentName());
+		
+		HiJava s2 = new HiJava();
+		s2.studentName = "¡÷¡÷";
+		System.out.println(s2.getStudentName());
+
+	}
+
+}

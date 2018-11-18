e5bde437acf7004cfd0c4e2f374add21741b7f51
heedeandean <7942ahj@hanmail.net>
2018-11-18 22:13:00 +0900

    클래스

--- Practice/src/study/HiJava.java
+++ Practice/src/study/HiJava.java
@@ -1,24 +1,10 @@
 package study;
 
 public class HiJava {
-	int studentID;
-	String studentName;
-	int grade;
-	String address;
-	
-	public String getStudentName() {
-		return studentName;
-	}
 
 	public static void main(String[] args) {
-		HiJava su = new HiJava();
-		su.studentName = "¾öÈñÁø";
-		    
-		System.out.println(su.studentName);
-		System.out.println(su.getStudentName());
-				
-				
-	
+		// TODO Auto-generated method stub
+
 	}
 
 }

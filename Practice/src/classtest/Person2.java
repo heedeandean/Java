f38c6ec9e5ca43032d057f7d54cb47d09b3323e7
heedeandean <7942ahj@hanmail.net>
2018-11-19 09:53:30 +0900

    생성자

--- Practice/src/classtest/Person2.java
+++ Practice/src/classtest/Person2.java
@@ -5,6 +5,8 @@ public class Person2 {
 	float height;
 	float weight;
 	
+	public Person2() {} // 디폴트 생성자
+	
 	public Person2(String uname, float uheight, float uweight) {
 		name = uname;
 		height = uheight;

c96c03b9f18fae9b278c8193b2588bf255e6b689
heedeandean <7942ahj@hanmail.net>
2018-11-20 09:44:24 +0900

    매개변수가 있는 생성자 추가

---
+++ Practice/src/classtest/Person2.java
@@ -0,0 +1,13 @@
+package classtest;
+
+public class Person2 {
+	String name;
+	float height;
+	float weight;
+	
+	public Person2(String uname, float uheight, float uweight) {
+		name = uname;
+		height = uheight;
+		weight = uweight;
+	}
+}

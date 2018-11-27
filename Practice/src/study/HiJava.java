11780bc18e74ca6b8921c3058ec825ffd513b320
heedeandean <7942ahj@hanmail.net>
2018-11-27 17:23:59 +0900

    사칙연산 함수

--- Practice/src/study/HiJava.java
+++ Practice/src/study/HiJava.java
@@ -1,103 +1,39 @@
 package study;
 
-import java.util.Calendar;
-
 public class HiJava {
-	private int day;
-	private int month;
-	private int year;
-	private boolean isValid = true;
-	
-	public HiJava(int day, int month, int year) {
-		setYear(year);
-		setMonth(month);
-		setDay(day);
-	}
-
-	public int getDay() {
-		return day;
-	}
-
-	public void setDay(int day) {
-		switch(month) {
-		 case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-			 if (day < 1 || day > 31) {
-				 isValid = false;
-			 }
-			 else {
-				 this.day = day;
-			 }
-			 break;
-			 
-		 case 4: case 6: case 9: case 11:
-			 if (day < 1 || day > 30) {
-				 isValid = false;
-			 }
-			 else {
-				 this.day = day;
-			 }
-			 break;
-			 
-		 case 2:
-			 if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-				 if (day < 1 || day > 29) {
-					 isValid = false;
-				 }
-				 else {
-					 this.day = day;
-				 }
-			 }
-			 else {
-				 if (day < 1 || day > 28) {
-					 isValid = false;
-				 }
-				 else {
-					 this.day = day;
-				 }
-			 }
-			 break;
-			 
-		default:
-			isValid = false;
-			break;
-		}
-	}
-
-	public int getMonth() {
-		return month;
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		
+		int result = add(num1, num2);
+		System.out.println(result);
+		
+		result = minus(num1, num2);
+		System.out.println(result);
+		
+		result = multiple(num1, num2);
+		System.out.println(result);
+		
+		double result_div = div(num1, num2);
+		System.out.println(result_div);
+		
 	}
 	
-	public void setMonth(int month) {
-		if (month < 1 || month > 12) {
-			isValid = false;
-		}
-		else {
-			this.month = month;
-		}
-	}
-
-	public int getYear() {
-		return year;
-	}
-
-	public void setYear(int year) {
-		if (year > Calendar.getInstance().get(Calendar.YEAR)) {
-			isValid = false;
-		}
-		else {
-			this.year = year;
-		}
+	public static int add(int n1, int n2) {
+		return n1 + n2;
 	}
 	
-	public String isValid() {
-		if (isValid) {
-			return "유효한 날짜입니다.";
-		}
-		else {
-			return "유효하지 않은 날짜입니다.";
-		}
+	public static int minus(int n1, int n2) {
+		return n1 - n2;
 	}
-
 	
-
+	public static int multiple(int n1, int n2) {
+		return n1 * n2;
+	}
+	
+	public static double div(double n1, double n2) {
+		return n1 / n2;
+	}
+	
+	
 }

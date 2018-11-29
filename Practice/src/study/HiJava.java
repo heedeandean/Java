195836156cf094f29c15c1a8c44cfce967263ae7
heedeandean <7942ahj@hanmail.net>
2018-11-29 18:14:44 +0900

    this 출력

--- Practice/src/study/HiJava.java
+++ Practice/src/study/HiJava.java
@@ -1,39 +1,24 @@
 package study;
 
+class Birth {
+	int day;
+	int month;
+	int year;
+	
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	public void printThis() {
+		System.out.println(this);
+	}
+}
+	
 public class HiJava {
 	public static void main(String[] args) {
-		int num1 = 10;
-		int num2 = 20;
-		
-		int result = add(num1, num2);
-		System.out.println(result);
-		
-		result = minus(num1, num2);
-		System.out.println(result);
-		
-		result = multiple(num1, num2);
-		System.out.println(result);
-		
-		double result_div = div(num1, num2);
-		System.out.println(result_div);
-		
+		Birth b = new Birth();
+		b.setYear(2018);
+		System.out.println(b);
+		b.printThis();
 	}
-	
-	public static int add(int n1, int n2) {
-		return n1 + n2;
-	}
-	
-	public static int minus(int n1, int n2) {
-		return n1 - n2;
-	}
-	
-	public static int multiple(int n1, int n2) {
-		return n1 * n2;
-	}
-	
-	public static double div(double n1, double n2) {
-		return n1 / n2;
-	}
-	
-	
 }

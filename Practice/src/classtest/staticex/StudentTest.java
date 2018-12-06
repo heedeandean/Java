46d46888f2eef592f940d5ea8a9764350dd79ada
heedeandean <7942ahj@hanmail.net>
2018-12-06 20:54:11 +0900

    static

--- Practice/src/classtest/staticex/StudentTest.java
+++ Practice/src/classtest/staticex/StudentTest.java
@@ -1,5 +1,15 @@
 package classtest.staticex;
 
 public class StudentTest {
-
+	public static void main(String[] args) {
+		Student hee = new Student();
+		hee.setStuName("Èñµòµò");
+		System.out.println(hee.serialNum);
+		hee.serialNum++;
+		
+		Student rara = new Student();
+		rara.setStuName("¶ó¶ó");
+		System.out.println(rara.serialNum);
+		rara.serialNum++;
+	}
 }

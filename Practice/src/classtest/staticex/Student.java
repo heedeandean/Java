79937dd48082121e55e45d8e0d77bf717e65821d
heedeandean <7942ahj@hanmail.net>
2018-12-07 21:30:27 +0900

    static

--- Practice/src/classtest/staticex/Student.java
+++ Practice/src/classtest/staticex/Student.java
@@ -1,11 +1,18 @@
 package classtest.staticex;
 
 public class Student {
-	public static int serialNum = 1;
+	public static int serialNum = 0;
 	public int stuID;
 	public String stuName;
 	public int grade;
 	public String addr;
+	
+	// 생성자.
+	public Student() {
+		
+		serialNum++; // 학생이 생성될 때 마다 증가.
+		stuID = serialNum;
+	}
 	
 	public String getStuName() {
 		return stuName;
--- Practice/src/classtest/staticex/StudentTest.java
+++ Practice/src/classtest/staticex/StudentTest.java
@@ -2,14 +2,16 @@ package classtest.staticex;
 
 public class StudentTest {
 	public static void main(String[] args) {
+		
 		Student hee = new Student();
 		hee.setStuName("Èñµòµò");
-		System.out.println(hee.serialNum);
-		hee.serialNum++;
+		System.out.println(Student.serialNum);
+		System.out.println(hee.stuName + " ÇÐ¹ø : " + hee.stuID);
+		
 		
 		Student rara = new Student();
 		rara.setStuName("¶ó¶ó");
-		System.out.println(rara.serialNum);
-		rara.serialNum++;
+		System.out.println(Student.serialNum);
+		System.out.println(rara.stuName + " ÇÐ¹ø : " + rara.stuID);
 	}
 }

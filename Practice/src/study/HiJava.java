ebfff14f985eea296c4d13ad208109ce85a64ab3
heedeandean <7942ahj@hanmail.net>
2018-12-09 02:25:47 +0900

    .

--- Practice/src/study/HiJava.java
+++ Practice/src/study/HiJava.java
@@ -2,13 +2,31 @@ package study;
 
 public class HiJava {
 	public static void main(String[] args) {
-		char gender = 'F';
+		int age = 60;
+		int charge;
 		
-		if (gender == 'F') {
-			System.out.println("여자");
+		if(age < 8) {
+			charge = 1000;
+			System.out.println("취학 전 아동입니다.");
+		}
+		else if(age < 14) {
+			charge = 2000;
+			System.out.println("초딩");
+		}
+		else if(age < 20) {
+			charge = 2500;
+			System.out.println("중, 고딩");
+		}
+		else if(age < 60){
+			charge = 3000;
+			System.out.println("성인");
 		}
 		else {
-			System.out.println("남자");
+			charge = 0;
+			System.out.println("노인");
 		}
+		
+		System.out.println("입장료는 " + charge + "원입니다.");
+		
 	}
 }

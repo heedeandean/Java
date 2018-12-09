8328281294d30f6dd63fe59a3c061832a164c235
heedeandean <7942ahj@hanmail.net>
2018-12-10 02:30:55 +0900

    .

--- Practice/src/study/HiJava.java
+++ Practice/src/study/HiJava.java
@@ -2,31 +2,26 @@ package study;
 
 public class HiJava {
 	public static void main(String[] args) {
-		int age = 60;
-		int charge;
+		int score = 100;
+		char grade;
 		
-		if(age < 8) {
-			charge = 1000;
-			System.out.println("취학 전 아동입니다.");
+		if (score >= 90) {
+			grade = 'A';
 		}
-		else if(age < 14) {
-			charge = 2000;
-			System.out.println("초딩");
+		else if (score >= 80) {
+			grade = 'B';
 		}
-		else if(age < 20) {
-			charge = 2500;
-			System.out.println("중, 고딩");
+		else if (score >= 70) {
+			grade = 'C';
 		}
-		else if(age < 60){
-			charge = 3000;
-			System.out.println("성인");
+		else if (score >= 60) {
+			grade = 'D';
 		}
 		else {
-			charge = 0;
-			System.out.println("노인");
+			grade = 'F';
 		}
 		
-		System.out.println("입장료는 " + charge + "원입니다.");
-		
+		System.out.println("학점은 " + grade + "입니다.");
+
 	}
 }

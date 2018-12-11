9c2f3728f0f5b46fe970b5f270af548f0eb8a41a
heedeandean <7942ahj@hanmail.net>
2018-12-12 02:43:26 +0900

    .

--- Practice/src/study/HiJava.java
+++ Practice/src/study/HiJava.java
@@ -2,27 +2,15 @@ package study;
 
 public class HiJava {
 	public static void main(String[] args) {
-		int floor = 3;
-		String store;
+		int num = 1;
+		int sum = 0;
 		
-		switch(floor) {
-		 case 1 : store = "약국";
-		 	break;
-		 case 2 : store = "정형외과";
-		 	break;
-		 case 3 : store = "피부과";
-		 	break;
-		 case 4 : store = "치과";
-		 	break;
-		 case 5 : store = "헬스 클럽";
-		 	break;
-		 default:
-			 store = "존재하지 않는 층";
-			 break;
+		while(num <= 10) {
+			sum += num;
+			num++;
 		}
-		
-		System.out.println(floor + "층은 " + store + "입니다.");
-		
+		System.out.println(sum);
+
 
 	}
 }

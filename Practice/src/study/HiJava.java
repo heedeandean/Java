ec892ae5f17daf06168e3e7384fbf9abbea02dbc
heedeandean <7942ahj@hanmail.net>
2018-12-16 03:25:29 +0900

    .

--- Practice/src/study/HiJava.java
+++ Practice/src/study/HiJava.java
@@ -2,16 +2,30 @@ package study;
 
 public class HiJava {
 	public static void main(String[] args) {
+		char o = '*';
+		float n1 = 10;
+		float n2 = 4;
+		float result;
 		
-		int num;
-		
-		for(num=1; num<=100; num++) {
-			if (num % 3 != 0) 
-				continue;
+		if (o == '+') {
+			result = n1 + n2;
 			
-			System.out.println(num);
+		}
+		else if (o == '-') {
+			result = n1 - n2;
+		}
+		else if (o == '*') {
+			result = n1 * n2;
+		}
+		else if (o == '/') {
+			result = n1 / n2;
+		}
+		else {
+			System.out.println("올바른 사칙연산이 아닙니다.");
+			return;
 		}
 		
+		System.out.println("결과는 " + result + "입니다.");
 
 
 	}

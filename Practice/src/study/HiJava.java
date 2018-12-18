b26db8d6d08cb8af41edf300d27d1296e8c4b926
heedeandean <7942ahj@hanmail.net>
2018-12-18 11:14:56 +0900

    2018-12-18

--- Practice/src/study/HiJava.java
+++ Practice/src/study/HiJava.java
@@ -2,27 +2,13 @@ package study;
 
 public class HiJava {
 	public static void main(String[] args) {
-		char o = '/';
-		float n1 = 10;
-		float n2 = 4;
-		float result;
-		
-		switch (o) {
-		 case '+' : result = n1 + n2;
-		 	break;
-		 case '-' : result = n1 - n2;
-		 	break;
-		 case '*' : result = n1 * n2;
-		 	break;
-		 case '/' : result = n1 / n2;
-		 	break;
-		 default:
-			 System.out.println("올바른 사칙연산이 아닙니다.");
-			 return;
+		for (int dan=2; dan<=9; dan++) {
+			for (int times=1; times<=9; times++) {
+				if (dan < times)
+					break;
+				System.out.println(dan + " x " + times + " = " + (dan * times));
+			}
+			System.out.println();
 		}
-		
-		System.out.println("결과는 " + result + "입니다.");
-
-		test
 	}
 }

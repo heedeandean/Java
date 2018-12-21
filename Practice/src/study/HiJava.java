a3b12f9d513edb85d0dfd6b8f42621eb4a9b2c4a
heedeandean <7942ahj@hanmail.net>
2018-12-21 15:41:15 +0900

    2018-12-21

--- Practice/src/study/HiJava.java
+++ Practice/src/study/HiJava.java
@@ -2,9 +2,40 @@ package study;
 
 public class HiJava {
 	public static void main(String[] args) {
-
-					
+		int num1 = 10;
+		int num2 = 20;
 		
+		int result = add(num1, num2);
+		System.out.println(result);
+		
+		result = sub(num1, num2);
+		System.out.println(result);
+		
+		result = mul(num1, num2);
+		System.out.println(result);
+		
+		double result_div = div(num1, num2);
+		System.out.println(result_div);
+
+	}
 	
+	public static int add(int n1, int n2) {
+		int result = n1 + n2;
+		return result;
+	}
+	
+	public static int sub(int n1, int n2) {
+		int result = n1 - n2;
+		return result;
+	}
+	
+	public static int mul(int n1, int n2) {
+		int result = n1 * n2;
+		return result;
+	}
+	
+	public static double div(double n1, double n2) {
+		double result = n1 / n2;
+		return result;
 	}
 }

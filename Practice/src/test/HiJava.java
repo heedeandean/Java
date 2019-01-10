e2464c0479067e14a8839ad1c962c4e70061fc33
heedeandean <7942ahj@hanmail.net>
2019-01-11 00:20:53 +0900

    2019-01-11

--- Practice/src/test/HiJava.java
+++ Practice/src/test/HiJava.java
@@ -3,16 +3,18 @@ package test;
 public class HiJava {
 
 	public static void main(String[] args) {
-		double[] data = new double[5];
-		int size = 0;
+		char[] alp = new char[26];
+		char ch = 'A';
 		
-		data[0] = 10.0; size++;
-		data[1] = 20.0; size++;
-		data[2] = 30.0; size++;
-		
-		for(int i=0; i<size; i++) {
-			System.out.println(data[i]);
+		for(int i=0; i< alp.length; i++, ch++) {
+			alp[i] = ch; 
 		}
+		
+		for(int i=0; i< alp.length; i++) {
+			System.out.println(alp[i]+", " + (int)alp[i]);
+		}
+		
+
 
 	}
 

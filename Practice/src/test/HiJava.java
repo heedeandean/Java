46f81576e1ddacb77b939a15fdcb930ed79ea9f6
heedeandean <7942ahj@hanmail.net>
2019-01-20 01:56:38 +0900

    2019-01-20

--- Practice/src/test/HiJava.java
+++ Practice/src/test/HiJava.java
@@ -3,15 +3,10 @@ package test;
 public class HiJava {
 
 	public static void main(String[] args) {
-		int[] array1 = {10,20,30,40,50};
-		int[] array2 = {1,2,3,4,5};
+		String[] strArray = {"매운족발", "허니오리지날", "단호박피자", "까르보나라", "까르보불닭"};
 		
-		System.arraycopy(array1, 0, array2, 1, 3);
-		
-		for(int i=0; i<array2.length; i++) {
-			System.out.println(array2[i]);
+		for(String food : strArray) {
+			System.out.println(food);
 		}
-
 	}
-
 }

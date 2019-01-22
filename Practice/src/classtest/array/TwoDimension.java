771f8770dd7b37d18531de94e3ac5731aeb5cbdd
heedeandean <7942ahj@hanmail.net>
2019-01-23 02:14:18 +0900

    2019-01-23

--- Practice/src/classtest/array/TwoDimension.java
+++ Practice/src/classtest/array/TwoDimension.java
@@ -4,6 +4,16 @@ public class TwoDimension {
 
 	public static void main(String[] args) {
 
+		char[][] alp = new char[13][2];
+		char ch = 'a';
+		
+		for(int i=0; i<alp.length; i++) {
+			for(int j=0; j<alp[i].length; j++) {
+				alp[i][j] = ch;
+				System.out.println(alp[i][j]);
+				ch++;
+			}
+			System.out.println();
+		}
 	}
-
 }

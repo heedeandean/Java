5bf72745680c397f864f1c13f01c9a8c7d67af96
heedeandean <7942ahj@hanmail.net>
2019-01-24 02:19:25 +0900

    2019-01-24

---
+++ Practice/src/test/test.java
@@ -0,0 +1,20 @@
+package test;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		char[][] alp = new char[13][2];
+		char ch = 'a';
+		
+		for(int i=0; i<alp.length; i++) {
+			for(int j=0; j<alp[i].length; j++) {
+				alp[i][j] = ch;
+				ch++;
+				
+				System.out.println(alp[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}

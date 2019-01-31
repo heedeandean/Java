1e29f562c18edce8bb7e0331e2fb4daf64880ec9
heedeandean <7942ahj@hanmail.net>
2019-02-01 00:38:14 +0900

    2019-02-01

---
+++ Practice/src/classtest/array/CharArray.java
@@ -0,0 +1,16 @@
+package classtest.array;
+
+public class CharArray {
+
+	public static void main(String[] args) {
+		
+		// 대문자 -> 소문자 
+		char[] alp = new char[26];
+		char ch= 'A';
+		
+		for(int i=0; i<alp.length; i++, ch++) {
+			alp[i] = (char)((int)ch + 32);
+			System.out.println(alp[i]);
+		}
+	}
+}

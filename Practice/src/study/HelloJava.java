659e85925c9d2d852f5604d3d8b61190f4c5250f
heedeandean <7942ahj@hanmail.net>
2019-03-07 03:26:14 +0900

    .

--- Practice/src/study/HelloJava.java
+++ Practice/src/study/HelloJava.java
@@ -3,14 +3,7 @@ package study;
 public class HelloJava {
 
 	public static void main(String[] args) {
-		int dan;
-		int times;
 		
-		for(dan=3; dan<=7; dan++) {
-			for(times=1; times<=9; times++) {
-				System.out.println(dan + " X " + times + " = " + (dan * times));
-			}
-			System.out.println();
-		}
+
 	}
 }

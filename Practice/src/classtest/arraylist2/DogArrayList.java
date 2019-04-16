9153376d7b9ab2f357b71be88901873e0ac2c10b
heedeandean <7942ahj@hanmail.net>
2019-04-16 22:15:55 +0900

    2019-04-16

--- Practice/src/classtest/arraylist2/DogArrayList.java
+++ Practice/src/classtest/arraylist2/DogArrayList.java
@@ -6,21 +6,21 @@ public class DogArrayList {
 
 	public static void main(String[] args) {
 		
-		ArrayList<Dog> dog = new ArrayList<Dog>();
+		ArrayList<Dog> dogList = new ArrayList<Dog>();
 		
-		dog.add(new Dog("모모", "웰시코기"));
-		dog.add(new Dog("라라", "불독"));
-		dog.add(new Dog("포포", "시바견"));
+		dogList.add(new Dog("모모", "웰시코기"));
+		dogList.add(new Dog("라라", "불독"));
+		dogList.add(new Dog("포포", "시바견"));
 		
-		for(int i=0; i<dog.size(); i++) {
-			System.out.println(dog.get(i).showDogInfo());
+		for(int i=0; i<dogList.size(); i++) {
+			System.out.println(dogList.get(i).showDogInfo());
 		}
 		
 		System.out.println();
 		System.out.println("=======향상된 for문=======");
 		
-		for(Dog dogs : dog) {
-			System.out.println(dogs.showDogInfo());
+		for(Dog dog : dogList) {
+			System.out.println(dog.showDogInfo());
 		}
 	}
 }

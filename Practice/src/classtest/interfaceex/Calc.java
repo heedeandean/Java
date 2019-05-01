07933d8cb4b3fd38a99e8f820130d97f0b1453da
heedeandean <7942ahj@hanmail.net>
2019-05-01 20:50:42 +0900

    2019-05-01

--- Practice/src/classtest/interfaceex/Calc.java
+++ Practice/src/classtest/interfaceex/Calc.java
@@ -13,6 +13,7 @@ public interface Calc {
 	// 디폴트 메서드
 	default void description() {
 		System.out.println("정수 계산기를 구현합니다.");
+		myMethod();
 	}
 	
 	// 정적 메서드
@@ -23,6 +24,15 @@ public interface Calc {
 			total += i;
 		}
 		
+		myStaticMethod();
 		return total;
+	}
+	
+	private void  myMethod() {
+		System.out.println("private 메서드입니다.");
+	}
+	
+	private static void myStaticMethod() {
+		System.out.println("private static 메서드입니다.");
 	}
 }

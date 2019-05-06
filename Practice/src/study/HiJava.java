517eaaee8908d063a34b1b83e2c973a223d92ddd
heedeandean <7942ahj@hanmail.net>
2019-05-06 11:30:42 +0900

    2019-05-06

--- Practice/src/study/HiJava.java
+++ Practice/src/study/HiJava.java
@@ -3,9 +3,7 @@ package study;
 public class HiJava {
 
 	public static void main(String[] args) {
-		int num1 = 2;
 		
-		System.out.println(~num1);
 
 
 	}
--- Practice/src/test/ST.java
+++ Practice/src/test/ST.java
@@ -3,6 +3,12 @@ package test;
 public class ST {
 
 	public static void main(String[] args) {
+		Student date1 = new Student(30, 2, 2000);
+		System.out.println(date1.isValid());
+		
+		Student date2 = new Student(2, 10, 2006);
+		System.out.println(date2.isValid());
+		
 
 	}
 

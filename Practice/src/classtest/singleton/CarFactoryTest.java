2e7149dd9a026eb241e9388c1cf9ccd314e8632b
heejin um <44691187+heedeandean@users.noreply.github.com>
2019-05-08 01:51:46 +0900

    Update CarFactoryTest.java

--- Practice/src/classtest/singleton/CarFactoryTest.java
+++ Practice/src/classtest/singleton/CarFactoryTest.java
@@ -3,7 +3,7 @@ package classtest.singleton;
 public class CarFactoryTest {
 
 	public static void main(String[] args) {
-		CarFactory factory = CarFactory.getInstance(); // 싱글톤 
+		CarFactory factory = CarFactory.getInstance(); // 싱글톤 패턴
 		
 		Car heeAudi = factory.createCar();
 		Car jinAudi = factory.createCar();

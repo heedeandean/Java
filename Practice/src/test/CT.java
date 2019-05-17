bd3f919aad8fee88dc022581bc698151ce47e69e
heedeandean <7942ahj@hanmail.net>
2019-05-17 15:06:50 +0900

    2019-05-17

---
+++ Practice/src/test/CT.java
@@ -0,0 +1,16 @@
+package test;
+
+public class CT {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		Person p = new Person();
+		Class pClass1 = p.getClass();
+		System.out.println(pClass1.getName());
+		
+		Class pClass2 = Person.class;
+		System.out.println(pClass2.getName());
+		
+		Class pClass3 = Class.forName("test.Person");
+		System.out.println(pClass3.getName());
+	}
+}

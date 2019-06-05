67451d65084229daca6a843e519b53314f012eef
heedeandean <7942ahj@hanmail.net>
2019-06-05 22:18:00 +0900

    2019-06-05

--- Practice/src/classtest/collectionframework/collection/arraylist/MemberArrayList.java
+++ Practice/src/classtest/collectionframework/collection/arraylist/MemberArrayList.java
@@ -27,14 +27,31 @@ public class MemberArrayList {
 	
 	// 회원 제거 메서드
 	public boolean removeMember(int memberId) {
+		
+//		// 방법1. Iterator(순서X, 순회)
+//		Iterator<Member> ir = arrayList.iterator();
+//		
+//		while(ir.hasNext()) {
+//			Member member = ir.next();
+//			
+//			int tempId = member.getMemberId();
+//			if(tempId == memberId) {
+//				arrayList.remove(member);
+//				return true;
+//			}
+//		}
+		
+		// 방법2. get()메서드(순서O, 순차적으로 하나씩 꺼냄)
 		for(int i=0; i<arrayList.size(); i++) {
 			Member member = arrayList.get(i);
+			
 			int tempId = member.getMemberId();
 			if(tempId == memberId) {
 				arrayList.remove(i);
 				return true;
 			}
 		}
+		
 		System.out.println(memberId + "가 존재하지 않습니다.");
 		return false;
 	}

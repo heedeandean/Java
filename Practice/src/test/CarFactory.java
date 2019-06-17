19510cef4eb14b330ad89ce8fcbd4fce06fbdb8c
heedeandean <7942ahj@hanmail.net>
2019-06-17 18:05:13 +0900

    2019-06-17

--- Practice/src/test/CarFactory.java
+++ Practice/src/test/CarFactory.java
@@ -1,5 +1,26 @@
 package test;
 
-public class CarFactory {
+import java.util.HashMap;
 
+public class CarFactory {
+	public static CarFactory instance = new CarFactory();
+	HashMap<String, Car> hm = new HashMap<>();
+	
+	public CarFactory() {}
+	
+	public static CarFactory getInstance() {
+		if(instance == null)
+			instance = new CarFactory();
+		return instance;
+	}
+	
+	public Car createCar(String name) {
+		if(hm.containsKey(name)) {
+			return hm.get(name);
+		}
+		Car car = new Car();
+		hm.put(name, car);
+		return car;
+	}
+	
 }
--- Practice/src/test/CarTest.java
+++ Practice/src/test/CarTest.java
@@ -1,10 +1,20 @@
 package test;
 
+
 public class CarTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		CarFactory fac = CarFactory.getInstance();
+		
+		Car v1 = fac.createCar("희진차");
+		Car v2 = fac.createCar("희진차");
+		System.out.println(v1 == v2);
+		
+		Car a1 = fac.createCar("아빠차");
+		Car a2 = fac.createCar("아빠차");
+		System.out.println(a1 == a2);
+		
+		System.out.println(v1 == a1);
+	
 	}
-
 }

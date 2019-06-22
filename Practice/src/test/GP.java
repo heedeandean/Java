3380fa8f8b70281edf44b8d867186a89bc70e0aa
heedeandean <7942ahj@hanmail.net>
2019-06-22 22:45:50 +0900

    2019-06-22

---
+++ Practice/src/test/GP.java
@@ -0,0 +1,17 @@
+package test;
+
+public class GP<T> {
+	private T material;
+
+	public T getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(T material) {
+		this.material = material;
+	}
+	
+	public String toString() {
+		return material.toString();
+	}
+}
---
+++ Practice/src/test/GPTest.java
@@ -0,0 +1,19 @@
+package test;
+
+public class GPTest {
+
+	public static void main(String[] args) {
+		var pP = new GP<Powder>();
+		
+		pP.setMaterial(new Powder());
+		Powder p = pP.getMaterial();
+		System.out.println(pP);
+		
+		GP<Plastic> plaP = new GP<Plastic>();
+		
+		plaP.setMaterial(new Plastic());
+		Plastic pla = plaP.getMaterial();
+		System.out.println(plaP);
+	}
+
+}

882f1e1be2a14b7abbfb032443bddf339d66e828
heedeandean <7942ahj@hanmail.net>
2019-06-26 21:26:35 +0900

    2019-06-26

---
+++ Practice/src/classtest/collectionframework/collection/treeset/TreeSetTest.java
@@ -0,0 +1,16 @@
+package classtest.collectionframework.collection.treeset;
+
+import java.util.TreeSet;
+
+public class TreeSetTest {
+	public static void main(String[] args) {
+		TreeSet<String> treeSet = new TreeSet<String>();
+		treeSet.add("김일수");
+		treeSet.add("김이수");
+		treeSet.add("김삼수");
+		
+		for(String str : treeSet) {
+			System.out.println(str);
+		}
+	}
+}

76f9a0fd3ef2c90de5b4da1b5c75e444681ad1db
heedeandean <7942ahj@hanmail.net>
2019-07-01 14:53:37 +0900

    2019-07-01

---
+++ Practice/src/test/MyNumTest.java
@@ -0,0 +1,10 @@
+package test;
+
+public class MyNumTest {
+
+	public static void main(String[] args) {
+		MyNumber max = (x, y) -> (x >= y) ? x : y;
+		System.out.println(max.getMax(10, 20));
+	}
+
+}

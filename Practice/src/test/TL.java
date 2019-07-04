8d4088ee8894389d6a8b8c293f9967457195ec4a
heedeandean <7942ahj@hanmail.net>
2019-07-04 21:51:40 +0900

    2019-07-04

---
+++ Practice/src/test/TL.java
@@ -0,0 +1,20 @@
+package test;
+
+interface PrintString {
+	void showString(String str);
+}
+
+public class TL {
+
+	public static void main(String[] args) {
+		PrintString l = s -> System.out.println(s);
+		l.showString("하이 람다1");
+		showMyString(l);
+
+	}
+
+	public static void showMyString(PrintString p) {
+		p.showString("하이 람다2");
+	}
+
+}

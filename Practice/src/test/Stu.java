39eac8c8ebb633ad6e8a7c2c4cb0e680e946e914
heedeandean <7942ahj@hanmail.net>
2019-07-07 13:50:56 +0900

    2019-07-07

---
+++ Practice/src/test/Stu.java
@@ -0,0 +1,36 @@
+package test;
+
+public class Stu {
+	String stuId;
+	String stuName;
+	
+	public Stu(String stuId, String stuName) {
+		this.stuId = stuId;
+		this.stuName = stuName;
+	}
+
+	@Override
+	public int hashCode() {
+		return Integer.parseInt(stuId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Stu) {
+			Stu s = (Stu)obj;
+			if(this.stuId == s.stuId)
+				return true;
+			else
+				return false;
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return stuId + ":" + stuName;
+	}
+	
+	
+	
+}

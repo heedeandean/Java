41ac16fb859094f64fabac152d78824ca48fd940
heedeandean <7942ahj@hanmail.net>
2019-07-08 20:43:58 +0900

    2019-07-08

--- Practice/src/test/ST.java
+++ Practice/src/test/ST.java
@@ -1,18 +1,19 @@
 package test;
 
+import java.util.HashSet;
+
 public class ST {
 
 	public static void main(String[] args) {
-		String s1 = "hihi";
-		String s2 = "heedeandean";
-		
-		CC c1 = new CC();
-		c1.makeString(s1, s2);
-		
-		// 람다식
-		StringConcat c2 = (s, v) -> System.out.println(s + ", " + v);
-		c2.makeString(s1, s2);
-		
+		HashSet<Stu> set = new HashSet<Stu>();
+
+		set.add(new Stu("1", "김일수"));
+		set.add(new Stu("2", "김이수"));
+		set.add(new Stu("3", "김삼수"));
+		set.add(new Stu("4", "김사수"));
+		set.add(new Stu("1", "김오수"));
+
+		System.out.println(set);
 	}
 
 }
--- Practice/src/test/StringConcat.java
+++
@@ -1,5 +0,0 @@
-package test;
-
-public interface StringConcat {
-	public void makeString(String s1, String s2);
-}

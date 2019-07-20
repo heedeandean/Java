dab736de500d7d9007d2334f8c113635bd2a2840
heedeandean <7942ahj@hanmail.net>
2019-07-20 11:05:56 +0900

    2019-07-20

---
+++ Practice/src/test/Cus.java
@@ -0,0 +1,30 @@
+package test;
+
+public class Cus {
+	private String name;
+	private int age;
+	private int price;
+
+	public Cus(String name, int age, int price) {
+		this.name = name;
+		this.age = age;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public String toString() {
+		return "name : " + name + "age : " + age + "price : " + price;
+	}
+
+}

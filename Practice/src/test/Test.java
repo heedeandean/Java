c2b930d353d61ba3548269395cc01c0ae71aa594
heedeandean <7942ahj@hanmail.net>
2019-07-20 09:59:41 +0900

    2019-07-20

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -1,17 +1,24 @@
 package test;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Test {
 
 	public static void main(String[] args) {
-		int[] arr = { 1, 2, 3, 4 };
+		List<String> sList = new ArrayList<String>();
+		sList.add("희딘");
+		sList.add("영신");
+		sList.add("유나");
 
-		int sumVal = Arrays.stream(arr).sum();
-		int count = (int) Arrays.stream(arr).count();
+		Stream<String> stream = sList.stream();
+		stream.forEach(s -> System.out.println(s + " "));
 
-		System.out.println(sumVal);
-		System.out.println(count);
+		System.out.println();
+
+		sList.stream().sorted().forEach(s -> System.out.println(s));
+
 	}
 
 }

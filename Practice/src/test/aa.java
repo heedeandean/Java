70adc6b50cdb7c2295d69b8629abb770892df977
heedeandean <7942ahj@hanmail.net>
2019-07-21 13:42:40 +0900

    2019-07-21

--- Practice/src/test/aa.java
+++ Practice/src/test/aa.java
@@ -1,26 +1,19 @@
 package test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class aa {
 
 	public static void main(String[] args) {
-		int[] arr = new int[5];
 
-//		try {
-//			for (int i = 0; i <= 5; i++) {
-//				arr[i] = i;
-//				System.out.println(arr[i]);
-//			}
-//		} catch (ArrayIndexOutOfBoundsException e) {
-//			System.out.println(e);
-//			System.out.println("예외 처리 부분");
-//
-//		}
-//		System.out.println("프로그램 종료");
-//	
-		for (int i = 0; i <= 5; i++) {
-			arr[i] = i;
-			System.out.println(arr[i]);
+		try {
+			FileInputStream fis = new FileInputStream("a.txt");
+		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+			System.out.println(e);
 		}
+		System.out.println("여기도 수행됩니다.");
 	}
 
 }

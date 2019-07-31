8ad53694f54704f6d4a5c4f11fbc787ca845b5f2
heedeandean <7942ahj@hanmail.net>
2019-07-31 09:56:22 +0900

    2019-07-31

---
+++ Practice/src/test/Test.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.io.IOException;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("알파벳 하나를 쓰고 [Enter]를 누르세요.");
+		
+		int i;
+		try {
+			i = System.in.read();
+			System.out.println(i);
+			System.out.println((char)i);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}

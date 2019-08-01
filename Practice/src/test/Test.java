8f0733c941336059ba8cbb7538ef3fae24b5f1cc
heedeandean <7942ahj@hanmail.net>
2019-08-01 10:06:23 +0900

    2019-08-01

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -4,14 +4,14 @@ import java.io.IOException;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("알파벳 하나를 쓰고 [Enter]를 누르세요.");
 		
 		int i;
 		try {
-			i = System.in.read();
-			System.out.println(i);
-			System.out.println((char)i);
+			while((i = System.in.read()) != -1) {
+				System.out.print((char)i);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}

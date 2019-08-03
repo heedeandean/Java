c5f734d176d23bbb65dfbf06fb514ec127dc25f5
heedeandean <7942ahj@hanmail.net>
2019-08-03 13:11:41 +0900

    2019-08-03

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -1,22 +1,24 @@
 package test;
 
-import java.util.Scanner;
+import java.io.Console;
 
 public class Test {
 
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
+		Console c = System.console();
 		
-		System.out.println("이름:");
-		String name = s.nextLine();
-		System.out.println("직업:");
-		String job = s.nextLine();
-		System.out.println("사번:");
-		int num = s.nextInt();
+		System.out.println("이름 : ");
+		String name = c.readLine();
+		System.out.println("직업 : ");
+		String job = c.readLine();
+		
+		System.out.println("비밀번호 : ");
+		char[] pass = c.readPassword();
+		String strPass = new String(pass);
 		
 		System.out.println(name);
 		System.out.println(job);
-		System.out.println(num);
-	}
+		System.out.println(strPass);
+	}	
 
 }

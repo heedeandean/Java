2c5a8d918ad361c3c1d13c56eb994850685af854
heedeandean <7942ahj@hanmail.net>
2019-08-04 13:33:15 +0900

    2019-08-04

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -1,24 +1,30 @@
 package test;
 
-import java.io.Console;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class Test {
 
 	public static void main(String[] args) {
-		Console c = System.console();
+		FileInputStream fis = null;
 		
-		System.out.println("이름 : ");
-		String name = c.readLine();
-		System.out.println("직업 : ");
-		String job = c.readLine();
-		
-		System.out.println("비밀번호 : ");
-		char[] pass = c.readPassword();
-		String strPass = new String(pass);
-		
-		System.out.println(name);
-		System.out.println(job);
-		System.out.println(strPass);
+		try {
+			fis = new FileInputStream("input.txt");
+			System.out.println((char)fis.read());
+			System.out.println((char)fis.read());
+			System.out.println((char)fis.read());
+		} catch (IOException e) {
+			System.out.println(e);
+		} finally {
+			try {
+				fis.close();
+			} catch (IOException e) {
+				System.out.println(e);
+			} catch (NullPointerException e) {
+				System.out.println(e);
+			}
+		}
+		System.out.println("end");
 	}	
 
 }

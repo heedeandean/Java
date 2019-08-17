c0669792be947c63556745beef7a39e1bf6dc87e
heedeandean <7942ahj@hanmail.net>
2019-08-17 23:57:18 +0900

    2019-08-17

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -1,31 +1,23 @@
 package test;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Test {
 
 	public static void main(String[] args) {
-		FileInputStream fis = null;
-		
-		try {
-			fis = new FileInputStream("input.txt");
-			System.out.println((char)fis.read());
-			System.out.println((char)fis.read());
-			System.out.println((char)fis.read());
-		} catch (IOException e) {
-			System.out.println(e);
-		} finally {
-			try {
-				fis.close();
-			} catch (IOException e) {
-				System.out.println(e);
-			} catch (NullPointerException e) {
-				System.out.println(e);
+		try (FileInputStream fis = new FileInputStream("input.txt")) {
+			int i;
+			while ((i = fis.read()) != -1) {
+				System.out.println((char) i);
 			}
+			System.out.println("end");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		// 되나요
-		System.out.println("end");
-	}	
+	}
 
 }

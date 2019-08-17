94b2b2cf8b9a19c72c2fa8878b258209b1e0a3e6
heedeandean <7942ahj@hanmail.net>
2019-08-18 00:06:25 +0900

    2019-08-18

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -1,23 +1,24 @@
 package test;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Test {
 
 	public static void main(String[] args) {
 		try (FileInputStream fis = new FileInputStream("input.txt")) {
+			byte[] bs = new byte[10];
 			int i;
-			while ((i = fis.read()) != -1) {
-				System.out.println((char) i);
+			while ((i = fis.read(bs)) != -1) {
+				for (byte b : bs) {
+					System.out.print((char) b);
+				}
+				System.out.println(": " + i + "바이트 읽음");
 			}
-			System.out.println("end");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		System.out.println("end");
 	}
 
 }

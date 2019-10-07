419fe9b1f690726743c90d9e6469e8453bd25fe9
heedeandean <7942ahj@hanmail.net>
2019-10-07 21:31:39 +0900

    2019-10-07

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class Test {
 
 	public static void main(String[] args) {
-		try (FileOutputStream fos = new FileOutputStream("output.txt")) {
+		try (FileOutputStream fos = new FileOutputStream("output.txt", true)) {
 			fos.write(65);
 			fos.write(66);
 			fos.write(67);

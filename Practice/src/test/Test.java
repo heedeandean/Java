8b96f25e947c081ca4bdd7fb0558ef14e7da99dd
heedeandean <7942ahj@hanmail.net>
2019-10-07 21:42:39 +0900

    2019-10-07

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -5,15 +5,20 @@ import java.io.IOException;
 
 public class Test {
 
-	public static void main(String[] args) {
-		try (FileOutputStream fos = new FileOutputStream("output.txt", true)) {
-			fos.write(65);
-			fos.write(66);
-			fos.write(67);
+	public static void main(String[] args) throws IOException {
+		FileOutputStream fos = new FileOutputStream("output2.txt", true);
+		try (fos) {
+			byte[] bs = new byte[26];
+			byte data = 65;
+			for (int i = 0; i < bs.length; i++) {
+				bs[i] = data;
+				data++;
+			}
+			fos.write(bs);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		System.out.println("출력이 완료되었습니다.");
+		System.out.println("출력완료");
 	}
 
 }

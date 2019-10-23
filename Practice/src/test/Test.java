3e3728c338e7b905d6c10411eb54b3884c035051
heedeandean <7942ahj@hanmail.net>
2019-10-23 21:06:34 +0900

    2019-10-23

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -4,17 +4,17 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
+//FileOutputStream과 OutputStreamWriter을 활용하여 a.txt 파일에 출력해 보기. 
+
 public class Test {
 
 	public static void main(String[] args) {
 		try (FileOutputStream fos = new FileOutputStream("a.txt");
 				OutputStreamWriter osw = new OutputStreamWriter(fos)) {
-			osw.write("빅데이터 가즈아");
+			osw.write("빅데이터!!!!!");
 		} catch (IOException e) {
 			System.out.println(e);
 		}
-
-		System.out.println("a.txt 파일을 열어보셈");
 
 	}
 

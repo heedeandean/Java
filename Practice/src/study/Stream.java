fb030c23ee268fe53ec4e9cee17b162b086143df
heedeandean <7942ahj@hanmail.net>
2019-10-27 15:23:16 +0900

    2019-10-27

--- Practice/src/study/Stream.java
+++ Practice/src/study/Stream.java
@@ -1,10 +1,20 @@
 package study;
 
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
 // FileOutputStream과 OutputStreamWriter을 활용하여 a.txt 파일에 출력해 보기. 
 
 public class Stream {
 
 	public static void main(String[] args) {
+		try (FileOutputStream fos = new FileOutputStream("a.txt");
+				OutputStreamWriter osw = new OutputStreamWriter(fos)) {
+			osw.write("빅데이터 가자!");
+		} catch (IOException e) {
+			System.out.println(e);
+		}
 
 	}
 
--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -1,20 +1,10 @@
 package test;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-
-//FileOutputStream과 OutputStreamWriter을 활용하여 a.txt 파일에 출력해 보기. 
 
 public class Test {
 
 	public static void main(String[] args) {
-		try (FileOutputStream fos = new FileOutputStream("a.txt");
-				OutputStreamWriter osw = new OutputStreamWriter(fos)) {
-			osw.write("빅데이터!!!!!");
-		} catch (IOException e) {
-			System.out.println(e);
-		}
+
 
 	}
 

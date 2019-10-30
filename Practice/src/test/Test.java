9096344fd440d9831392fb70d1fa84390e7e1748
heedeandean <7942ahj@hanmail.net>
2019-10-30 20:25:36 +0900

    2019-10-30

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -1,11 +1,78 @@
 package test;
 
-
 public class Test {
 
+	School heeSchool = School.getInstance();
+	Subject korean;
+	Subject math;
+
+	GenerateGradeReport gradeReport = new GenerateGradeReport();
+
 	public static void main(String[] args) {
+		Test test = new Test();
 
+		test.creatSub();
+		test.creatStu();
 
+		String report = test.gradeReport.getReport();
+		System.out.println(report);
+
+	}
+
+	public void creatSub() {
+		korean = new Subject("국어", Define.KOR);
+		math = new Subject("수학", Define.MATH);
+
+		heeSchool.addSubject(korean);
+		heeSchool.addSubject(math);
+
+	}
+
+	public void creatStu() {
+		Student s1 = new Student(1, "김일수", korean);
+		Student s2 = new Student(2, "김이수", math);
+		Student s3 = new Student(3, "김삼수", korean);
+		Student s4 = new Student(4, "김사수", korean);
+		Student s5 = new Student(5, "김오수", math);
+
+		heeSchool.addStudent(s1);
+		heeSchool.addStudent(s2);
+		heeSchool.addStudent(s3);
+		heeSchool.addStudent(s4);
+		heeSchool.addStudent(s5);
+
+		korean.register(s1);
+		korean.register(s2);
+		korean.register(s3);
+		korean.register(s4);
+		korean.register(s5);
+
+		math.register(s1);
+		math.register(s2);
+		math.register(s3);
+		math.register(s4);
+		math.register(s5);
+
+		addScoreForStu(s1, korean, 95);
+		addScoreForStu(s1, math, 56);
+
+		addScoreForStu(s2, korean, 95);
+		addScoreForStu(s2, math, 95);
+
+		addScoreForStu(s3, korean, 100);
+		addScoreForStu(s3, math, 88);
+
+		addScoreForStu(s4, korean, 89);
+		addScoreForStu(s4, math, 95);
+
+		addScoreForStu(s5, korean, 85);
+		addScoreForStu(s5, math, 56);
+
+	}
+
+	public void addScoreForStu(Student stu, Subject sub, int point) {
+		Score score = new Score(stu.getStuId(), sub, point);
+		stu.addSubjectScore(score);
 	}
 
 }

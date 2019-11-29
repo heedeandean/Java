95a65a8da75aae21c1282805f9b54906ff86e624
heedeandean <7942ahj@hanmail.net>
2019-11-29 21:36:55 +0900

    2019-11-29

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -3,9 +3,16 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-		int age;
-		age = 20;
-		System.out.println(age);
+		char ch1 = 'Z';
+		System.out.println(ch1);
+		System.out.println((int)ch1);
+	
+		char ch2 = 38;
+		System.out.println(ch2);
+		
+		int ch3 = 97;
+		System.out.println(ch3);
+		System.out.println((char)ch3);
 	}
 
 }

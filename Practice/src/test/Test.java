364bd656b0f48e57cb3f00f33dfcb365a66daffe
heedeandean <7942ahj@hanmail.net>
2019-11-29 22:29:50 +0900

    2019-11-29

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -3,21 +3,10 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-		byte bNum = 10;
-		int iNum = bNum;
+		int iNum = 10;
+		double dNum = 2.0;
 
-		System.out.println(bNum);
-		System.out.println(iNum);
-
-		int iNum2 = 20;
-		float fNum = iNum2;
-
-		System.out.println(iNum);
-		System.out.println(fNum);
-
-		double dNum;
-		dNum = fNum + iNum;
-		System.out.println(dNum);
+		System.out.println((int) (iNum + dNum));
 
 	}
 

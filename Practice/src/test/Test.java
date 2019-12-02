1652fc78335fb1348acd13b51042eadb6b5aed95
heedeandean <7942ahj@hanmail.net>
2019-12-02 21:32:08 +0900

    2019-12-02

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -5,12 +5,11 @@ public class Test {
 	public static void main(String[] args) {
 		int iNum = 10;
 		double dNum = 2.0;
-
-		System.out.println((int) (iNum + dNum));
-		System.out.println((int) (iNum - dNum));
-		System.out.println((int) (iNum / dNum));
-		System.out.println((int) (iNum * dNum));
-
+		
+		System.out.println((int)(iNum + dNum));
+		System.out.println((int)(iNum - dNum));
+		System.out.println((int)(iNum * dNum));
+		System.out.println((int)(iNum / dNum));
 	}
 
 }

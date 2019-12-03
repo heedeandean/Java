efcb0d04548b59759748f79743cb26a07b4b11f2
heedeandean <7942ahj@hanmail.net>
2019-12-03 22:33:10 +0900

    2019-12-03

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -4,42 +4,15 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		int num1 = 10;
-		int num2 = 2;
-		char operator = '*';
-		int result;
-
-//		if (operator == '+') {
-//			result = num1 + num2;
-//		} else if (operator == '-') {
-//			result = num1 - num2;
-//		} else if (operator == '*') {
-//			result = num1 * num2;
-//		} else if (operator == '/') {
-//			result = num1 / num2;
-//		} else {
-//			System.out.println("존재하는 사칙연산이 아닙니다.");
-//			return;
-//		}
-
-		switch (operator) {
-		case '+':
-			result = num1 + num2;
-			break;
-		case '-':
-			result = num1 - num2;
-			break;
-		case '*':
-			result = num1 * num2;
-			break;
-		case '/':
-			result = num1 / num2;
-			break;
-		default:
-			System.out.println("존재하는 사칙연산이 아닙니다.");
-			return;
+		for (int dan = 2; dan <= 8; dan++) {
+			if (dan % 2 != 0) {
+				continue;
+			}
+			for (int times = 1; times <= 9; times++) {
+				System.out.println(dan + " x " + times + " = " + (dan * times));
+			}
+			System.out.println();
 		}
-		System.out.println(result);
 
 	}
 

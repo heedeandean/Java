9c96b0074f584ceb9589891c82eaadebc7c2fb72
heedeandean <7942ahj@hanmail.net>
2019-12-03 22:36:00 +0900

    2019-12-03

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -4,11 +4,10 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		for (int dan = 2; dan <= 8; dan++) {
-			if (dan % 2 != 0) {
-				continue;
-			}
+		for (int dan = 2; dan <= 9; dan++) {
 			for (int times = 1; times <= 9; times++) {
+				if (dan < times)
+					break;
 				System.out.println(dan + " x " + times + " = " + (dan * times));
 			}
 			System.out.println();

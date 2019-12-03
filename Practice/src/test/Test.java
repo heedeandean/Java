3791e6df8d512459111316270c5a919a204c16b7
heedeandean <7942ahj@hanmail.net>
2019-12-03 22:01:13 +0900

    2019-12-03

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -3,31 +3,10 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-		int floor = 3;
-		String store;
 
-		switch (floor) {
-		case 1:
-			store = "약국";
-			break;
-		case 2:
-			store = "정형외과";
-			break;
-		case 3:
-			store = "피부과";
-			break;
-		case 4:
-			store = "치과";
-			break;
-		case 5:
-			store = "헬스 클럽";
-			break;
-		default:
-			System.out.println("존재하지 않는 층입니다.");
-			return;
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("안녕하세요" + i);
 		}
-		System.out.println(floor + "층 " + store + "입니다.");
-
 	}
 
 }

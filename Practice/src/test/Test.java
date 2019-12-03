a531ca91a257c50ee09a872cbb35efa936cdf649
heedeandean <7942ahj@hanmail.net>
2019-12-03 21:41:57 +0900

    2019-12-03

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -3,22 +3,30 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-		int score = 100;
-		char grade;
+		int floor = 3;
+		String store;
 
-		if (score >= 90) {
-			grade = 'A';
-		} else if (score >= 80) {
-			grade = 'B';
-		} else if (score >= 70) {
-			grade = 'C';
-		} else if (score >= 60) {
-			grade = 'D';
-		} else {
-			grade = 'F';
+		switch (floor) {
+		case 1:
+			store = "약국";
+			break;
+		case 2:
+			store = "정형외과";
+			break;
+		case 3:
+			store = "피부과";
+			break;
+		case 4:
+			store = "치과";
+			break;
+		case 5:
+			store = "헬스 클럽";
+			break;
+		default:
+			System.out.println("존재하지 않는 층입니다.");
+			return;
 		}
-
-		System.out.println(grade + "입니다.");
+		System.out.println(floor + "층 " + store + "입니다.");
 
 	}
 

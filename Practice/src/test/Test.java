0704403445cd7b8c3fe42478da4cfec19558ba78
heedeandean <7942ahj@hanmail.net>
2019-12-03 22:29:25 +0900

    2019-12-03

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -4,15 +4,42 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		int sum = 0;
-		int num;
-		for (num = 1;; num++) {
-			sum += num;
-			if (sum > 500)
-				break;
+		int num1 = 10;
+		int num2 = 2;
+		char operator = '*';
+		int result;
+
+//		if (operator == '+') {
+//			result = num1 + num2;
+//		} else if (operator == '-') {
+//			result = num1 - num2;
+//		} else if (operator == '*') {
+//			result = num1 * num2;
+//		} else if (operator == '/') {
+//			result = num1 / num2;
+//		} else {
+//			System.out.println("존재하는 사칙연산이 아닙니다.");
+//			return;
+//		}
+
+		switch (operator) {
+		case '+':
+			result = num1 + num2;
+			break;
+		case '-':
+			result = num1 - num2;
+			break;
+		case '*':
+			result = num1 * num2;
+			break;
+		case '/':
+			result = num1 / num2;
+			break;
+		default:
+			System.out.println("존재하는 사칙연산이 아닙니다.");
+			return;
 		}
-		System.out.println(sum);
-		System.out.println(num);
+		System.out.println(result);
 
 	}
 

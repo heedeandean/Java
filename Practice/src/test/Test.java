8948dc61ea5bb11051b3cb0615c5e1582c97c30d
heedeandean <7942ahj@hanmail.net>
2019-12-04 20:38:16 +0900

    2019-12-04

--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -3,26 +3,42 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-		int lineCount = 4;
-		int spaceCount = lineCount / 2 + 1;
-		int starCount = 1;
 
-		for (int i = 0; i < lineCount; i++) {
-			for (int j = 0; j < spaceCount; j++) {
-				System.out.print(' ');
-			}
-			for (int j = 0; j < starCount; j++) {
-				System.out.print('*');
-			}
-			for (int j = 0; j < spaceCount; j++) {
-				System.out.print(" ");
-			}
-			spaceCount -= 1;
-			starCount += 2;
-			System.out.println();
+		int num1 = 10;
+		int num2 = 20;
 
-		}
+		int sum = add(num1, num2);
+		System.out.println(num1 + " + " + num2 + " = " + sum + "입니다.");
 
+		int minus = minus(num1, num2);
+		System.out.println(num1 + " - " + num2 + " = " + minus + "입니다.");
+
+		int multiplied = multiplied(num1, num2);
+		System.out.println(num1 + " * " + num2 + " = " + multiplied + "입니다.");
+
+		double div = div(num1, num2);
+		System.out.println(num1 + " / " + num2 + " = " + div + "입니다.");
+
+	}
+
+	public static int add(int n1, int n2) {
+		int result = n1 + n2;
+		return result;
+	}
+
+	public static int minus(int n1, int n2) {
+		int result = n1 - n2;
+		return result;
+	}
+
+	public static int multiplied(int n1, int n2) {
+		int result = n1 * n2;
+		return result;
+	}
+
+	public static double div(double n1, double n2) {
+		double result = n1 / n2;
+		return result;
 	}
 
 }

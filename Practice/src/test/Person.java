48de0030e12c1efa2778d8122cbb020878207957
heedeandean <7942ahj@hanmail.net>
2019-12-05 23:09:22 +0900

    2019-12-05

---
+++ Practice/src/test/Person.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Person {
+	String name;
+	float height;
+	float weight;
+
+	public Person(String pname, float pheight, float pweight) {
+		this.name = pname;
+		this.height = pheight;
+		this.weight = pweight;
+	}
+}
--- Practice/src/test/Test.java
+++ Practice/src/test/Test.java
@@ -1,26 +1,9 @@
 package test;
 
 public class Test {
-
-	String orderNo;
-	String cusId;
-	String orderDate;
-	String cusName;
-	String productNo;
-	String cusAddr;
-	
 	public static void main(String[] args) {
-		Test a = new Test();
-		
-		a.orderNo = "20191205";
-		a.cusId = "heedeandean";
-		a.orderDate = "2019년 12월 05일";
-		a.cusName = "희딘딘";
-		a.productNo = "PD0345-12";
-		a.cusAddr = "서울시 마포구";
-
-		System.out.println("주문번호 : " + a.orderNo);
-
+		Person um = new Person("희딘딘", 163, 55);
+		System.out.println(um.name + um.height + um.weight);
 	}
 
 }

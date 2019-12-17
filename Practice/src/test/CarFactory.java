3c1fc2077b682f11310f6e517465f9ceb5ec0a88
heedeandean <7942ahj@hanmail.net>
2019-12-18 06:17:04 +0900

    2019-12-18

--- Practice/src/test/CarFactory.java
+++ Practice/src/test/CarFactory.java
@@ -1,7 +1,22 @@
 package test;
 
 public class CarFactory {
+	private static CarFactory instance = new CarFactory();
 
+	private CarFactory() {
+	}
 
+	public static CarFactory getInstance() {
+		if (instance == null) {
+			instance = new CarFactory();
+		}
 
+		return instance;
+	}
+
+	public Car createCar() {
+		Car car = new Car();
+
+		return car;
+	}
 }
--- Practice/src/test/CarFactoryTest.java
+++ Practice/src/test/CarFactoryTest.java
@@ -3,7 +3,13 @@ package test;
 public class CarFactoryTest {
 
 	public static void main(String[] args) {
+		CarFactory factory = CarFactory.getInstance();
 
+		Car c1 = factory.createCar();
+		Car c2 = factory.createCar();
+
+		System.out.println(c1.carNum);
+		System.out.println(c2.carNum);
 	}
 
 }

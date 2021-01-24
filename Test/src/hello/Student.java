39b9c8d9c1d6075737729ccde1aef32e43c91023
heedeandean <7942ahj@hanmail.net>
2021-01-25 02:31:36 +0900

    2021-01-25

--- Test/src/hello/Student.java
+++ Test/src/hello/Student.java
@@ -1,33 +1,5 @@
 package hello;
 
 public class Student {
-	private String stuID;
-	private String name;
-	
-	public Student() {}
-	
-	public Student(String stuID, String name) {
-		this.stuID = stuID;
-		this.name = name;
-	}
 
-	public String getstuID() {
-		return stuID;
-	}
-
-	public void setstuID(String stuID) {
-		this.stuID = stuID;
-	}
-
-	public String getname() {
-		return name;
-	}
-
-	public void setname(String name) {
-		this.name = name;
-	}
-	
-	public void showStudentInfo() {
-		System.out.println(stuID + ", " + name);
-	}
 }

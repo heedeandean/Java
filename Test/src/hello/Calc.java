43db0fdd3bae28e893c6ee4f3058d9f10de36d6a
heedeandean <7942ahj@hanmail.net>
2021-02-04 22:30:26 +0900

    2021-02-04

---
+++ Test/src/hello/Calc.java
@@ -0,0 +1,12 @@
+package hello;
+
+public interface Calc {
+	double PI = 3.14;
+	int ERROR = -999999999;
+	
+	int add(int n1, int n2);
+	int sub(int n1, int n2);
+	int times(int n1, int n2);
+	int divide(int n1, int n2);
+	
+}
